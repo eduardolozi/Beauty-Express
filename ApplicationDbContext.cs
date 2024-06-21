4dd54cfda8cee44af71dd78849f4050266206040
Bruno Gomes <brunogomes2703@outlook.com.br>
2024-06-20 22:07:29 -0300

    Adicionar arquivos de projeto.

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using BeautyExpress.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<DtoEstabelecimento> Estabelecimento { get; set; }
+}
---
+++ Models/DtoEstabelecimento.cs
@@ -0,0 +1,13 @@
+﻿namespace BeautyExpress.Models
+{
+    public class DtoEstabelecimento
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Endereco { get; set; }
+
+        public string Telefone { get; set; }
+    }
+}
